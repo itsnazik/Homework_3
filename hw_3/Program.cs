a4e5705a2003a3f0099fd92be9f4e19c9c536dcf
Nazik <nazikbaktygulova77@gmail.com>
2023-02-19 13:49:37 +0600

    Написан код для задачи №3 в папке hw_3

---
+++ hw_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход число N и выдает таблицу кубов чисел от 1 до N
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string Cube(int N) {
+    int index = 1;
+
+    while(index <= N) {
+       int i = index*index*index;
+       Console.Write(i + " ");
+       index++;
+    }
+    Console.WriteLine();
+    return "Done!";
+}
+
+Console.WriteLine(Cube(N));
